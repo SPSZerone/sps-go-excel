b37d5ca454ed629718d5cc0ac4a52910e33e8a9a
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-03-03 21:52:55 +0800

    gio

--- gui/page/diff/diff.go
+++ gui/page/diff/diff.go
@@ -7,8 +7,8 @@ import (
 	"gioui.org/widget/material"
 	"gioui.org/x/component"
 
-	"github.com/SPSZerone/sps-go-zerone/graphics/gio"
-	spslayout "github.com/SPSZerone/sps-go-zerone/graphics/gio/layout"
+	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
+	spsicon "github.com/SPSZerone/sps-go-zerone/graphics/gio/icon"
 )
 
 type Diff struct {
@@ -19,55 +19,35 @@ type Diff struct {
 	reloadButton widget.Clickable
 }
 
-func (d *Diff) Layout(app *gio.Application, gtx layout.Context, w *app.Window, th *material.Theme) layout.Dimensions {
+func (d *Diff) Layout(app *spsgio.Application, gtx layout.Context, w *app.Window, th *material.Theme) layout.Dimensions {
 	d.List.Axis = layout.Vertical
 	return material.List(th, &d.List).Layout(gtx, 1, func(gtx layout.Context, _ int) layout.Dimensions {
 		return layout.Flex{Axis: layout.Vertical}.Layout(
 			gtx,
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return spslayout.FlexInset{
-					Ratio: 0.8,
-					Flex: layout.Flex{
-						Axis:      layout.Horizontal,
-						Alignment: layout.Baseline,
-						Spacing:   layout.SpaceAround,
-					},
-				}.LayoutWidgets(
+				return layout.Flex{
+					Axis:      layout.Horizontal,
+					Alignment: layout.Middle,
+					Spacing:   layout.SpaceEvenly,
+					WeightSum: 1,
+				}.Layout(
 					gtx,
-					func() (float32, layout.Widget) {
-						return 0.7, func(gtx layout.Context) layout.Dimensions {
-							return d.fileInput.Layout(gtx, th, "File(xlsx,csv...)")
+					layout.Flexed(0.95, func(gtx layout.Context) layout.Dimensions {
+						return d.fileInput.Layout(gtx, th, "File(xlsx,csv...)")
+					}),
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						if d.fileButton.Clicked(gtx) {
+							app.Logger.Info().Msg("Open File")
 						}
-					},
-					func() (float32, layout.Widget) {
-						return 0.15, func(gtx layout.Context) layout.Dimensions {
-							if d.fileButton.Clicked(gtx) {
-								app.Logger.Info().Msg("Open File")
-							}
-							return material.Button(th, &d.fileButton, "Open File").Layout(gtx)
+						return material.IconButton(th, &d.fileButton, spsicon.ActionOpenInNew, "Open File").Layout(gtx)
+					}),
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						if d.reloadButton.Clicked(gtx) {
+							app.Logger.Info().Msg("Reload")
 						}
-					},
-					func() (float32, layout.Widget) {
-						return 0.15, func(gtx layout.Context) layout.Dimensions {
-							if d.reloadButton.Clicked(gtx) {
-								app.Logger.Info().Msg("Reload")
-							}
-							return material.Button(th, &d.reloadButton, "Reload").Layout(gtx)
-						}
-					},
+						return material.IconButton(th, &d.reloadButton, spsicon.ActionUpdate, "Reload").Layout(gtx)
+					}),
 				)
-				//}.LayoutABWidget(
-				//	gtx,
-				//	func(gtx layout.Context) layout.Dimensions {
-				//		return d.fileInput.Layout(gtx, th, "File(xlsx,csv...)")
-				//	},
-				//	func(gtx layout.Context) layout.Dimensions {
-				//		if d.fileButton.Clicked(gtx) {
-				//			app.Logger.Info().Msg("Open File")
-				//		}
-				//		return material.Button(th, &d.fileButton, "Open File").Layout(gtx)
-				//	},
-				//)
 			}),
 		)
 	})
--- gui/page/diff/page.go
+++ gui/page/diff/page.go
@@ -7,23 +7,23 @@ import (
 	"gioui.org/widget/material"
 	"gioui.org/x/component"
 
-	"github.com/SPSZerone/sps-go-zerone/graphics/gio"
-	"github.com/SPSZerone/sps-go-zerone/graphics/gio/icon"
+	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
+	spsicon "github.com/SPSZerone/sps-go-zerone/graphics/gio/icon"
 	spslayout "github.com/SPSZerone/sps-go-zerone/graphics/gio/layout"
 )
 
-var _ gio.Page = (*Page)(nil)
+var _ spsgio.Page = (*Page)(nil)
 
 type Page struct {
 	widget.List
-	*gio.Pages
+	*spsgio.Pages
 
 	split spslayout.Split
 	left  Diff
 	right Diff
 }
 
-func New(pages *gio.Pages) *Page {
+func New(pages *spsgio.Pages) *Page {
 	return &Page{
 		Pages: pages,
 	}
@@ -40,11 +40,11 @@ func (p *Page) Overflow() []component.OverflowAction {
 func (p *Page) NavItem() component.NavItem {
 	return component.NavItem{
 		Name: "Diff",
-		Icon: icon.ActionCompareArrows,
+		Icon: spsicon.ActionCompareArrows,
 	}
 }
 
-func (p *Page) Layout(app *gio.Application, gtx layout.Context, w *app.Window, th *material.Theme) layout.Dimensions {
+func (p *Page) Layout(app *spsgio.Application, gtx layout.Context, w *app.Window, th *material.Theme) layout.Dimensions {
 	p.List.Axis = layout.Vertical
 	return material.List(th, &p.List).Layout(gtx, 1, func(gtx layout.Context, _ int) layout.Dimensions {
 		return layout.Flex{
