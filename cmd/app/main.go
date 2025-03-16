1beef0e4b3da85cb3caf414c22cd40caa2d9285d
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-03-16 22:10:28 +0800

    gio

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
-	spspref "github.com/SPSZerone/sps-go-zerone/graphics/gio/tab/pref"
-
-	"github.com/SPSZerone/sps-go-excel/gui/tab/about"
-	"github.com/SPSZerone/sps-go-excel/gui/tab/diff"
+	"github.com/SPSZerone/sps-go-excel/gui"
 )
 
 func main() {
-	spsgio.Run(
-		spsgio.OptTitle("SPS Excel Tools"),
-		spsgio.OptOnInit(func(app *spsgio.Application) {
-			app.Logger.Info().Msg("SPS Excel Tools Init")
-			app.Tabs.Register(0, about.New(&app.Tabs))
-			app.Tabs.Register(1, spspref.New(&app.Tabs))
-			app.Tabs.Register(2, diff.New(&app.Tabs))
-			app.Tabs.SwitchTo(2)
-		}),
-		spsgio.OptOnStart(func(app *spsgio.Application) {
-			app.Logger.Info().Msg("SPS Excel Tools Start")
-		}),
-		spsgio.OptOnLoop(func(app *spsgio.Application) error {
-			app.Logger.Info().Msg("SPS Excel Tools Loop")
-			for {
-				destroy, err := app.OnEvent(app.Window.Event())
-				if destroy {
-					return err
-				}
-			}
-		}),
-		spsgio.OptOnStop(func(app *spsgio.Application) {
-			app.Logger.Info().Msg("SPS Excel Tools Stop")
-		}),
-	)
+	gui.Run()
 }
--- gui/application.go
+++ gui/application.go
@@ -1,4 +1,42 @@
 package gui
 
+import (
+	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
+	spspref "github.com/SPSZerone/sps-go-zerone/graphics/gio/tab/pref"
+
+	"github.com/SPSZerone/sps-go-excel/gui/tab/about"
+	"github.com/SPSZerone/sps-go-excel/gui/tab/diff"
+)
+
+func Run() {
+	spsgio.Run(
+		spsgio.OptTitle("SPS Excel Tools"),
+		spsgio.OptOnInit(func(app *spsgio.Application) {
+			app.Logger.Info().Msg("SPS Excel Tools Init")
+			app.Tabs.Register(0, about.New(&app.Tabs))
+			app.Tabs.Register(1, spspref.New(&app.Tabs))
+			app.Tabs.Register(2, diff.New(&app.Tabs))
+			app.Tabs.SwitchTo(2)
+		}),
+		spsgio.OptOnStart(func(app *spsgio.Application) {
+			app.Logger.Info().Msg("SPS Excel Tools Start")
+		}),
+		spsgio.OptOnLoop(onLoop),
+		spsgio.OptOnStop(func(app *spsgio.Application) {
+			app.Logger.Info().Msg("SPS Excel Tools Stop")
+		}),
+	)
+}
+
+func onLoop(app *spsgio.Application) error {
+	app.Logger.Info().Msg("SPS Excel Tools Loop")
+	for {
+		destroy, err := app.OnEvent(app.Window.Event())
+		if destroy {
+			return err
+		}
+	}
+}
+
 type Application struct {
 }
